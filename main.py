660fd78f74a4a8c4e59d229f8bf21fafcd30a87b
juanroder <60340193+juanroder@users.noreply.github.com>
2020-11-26 09:32:03 +0100

    Bewegungsverbersserung

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 # This ist a Shoot em up game coded by Juan Rodriguez
-import pygame
+import pygame, sys, time
+from pygame.locals import *
+
+pygame.init()
+FPS = 120
+
 
 # Initialize the Pygame.
 pygame.init()
 
 #  classes
 #  ----------------------------- Objects -------------------------------------
-#  Background
-background = pygame.image.load('BG_SHIP.jpeg')
+clock = pygame.time.Clock()
+
+background = pygame.image.load('BGBig1600.png')
 
 # Create the Screen.
-screen = pygame.display.set_mode((1600, 900))
+screenWidth = 1600
+screenHeight = 900
+screen = pygame.display.set_mode((screenWidth, screenHeight))
 
 # Title and icon on the window bar mas 32px
 pygame.display.set_caption("Razorcop")  # set windows Title.
@@ -19,12 +27,17 @@ pygame.display.set_icon(icon)   # set icon.
 
 # La nave de Razorcop!
 playerImg = pygame.image.load("Player_Ship.png")  # Image Loading
-playerX = 735  # places the x coord. for PlayerImg object in the middle
+playerX = 730  # places the x coord. for PlayerImg object in the middle
 playerY = 700  # places the y coord. for PlayerImg object in the middle
-playerX_change = 0  # New coordinate point for X
+playerX_change = 0 # New coordinate point for X
 playerY_change = 0  # New coordinate point for y
+player_left = False
+player_right = False
+player_up = False
+player_down = False
+player_vel = 10
 
-def player(x,y):
+def player(x, y):
     screen.blit(playerImg, (x, y))  # The term used for rendering objects is blitting
 
 
@@ -35,29 +48,35 @@ def player(x,y):
 # Game Loop
 running = True
 while running:  # While running ist True, window stay open.
+    clock.tick(FPS)
     screen.fill((0, 0, 0))  # Python needs updates from these changes, so we need a method for it.
     # Background Image
     screen.blit(background, (0, 0))
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # take care about the QUIT characters (activates when the [x] button on window)
             running = False
         # Movement
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_LEFT:
-            playerX_change -= 0.01
-        if event.key == pygame.K_RIGHT:
-            playerX_change += 0.01
-        if event.key == pygame.K_UP:
-            playerY_change -= 0.01
-        if event.key == pygame.K_DOWN:
-            playerY_change += 0.01
-    if event.type == pygame.KEYUP:
-        if event.key == pygame.K_LEFT and event.key == pygame.K_RIGHT:
-            playerX_change = 0.0
 
-    playerX += playerX_change
-    playerY += playerY_change
-    player(playerX, playerY)  # Function  for Player_ship will be called here
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and playerX > player_vel:
+        playerX -= player_vel
+        player_left = True
+        player_right = False
+    elif keys[pygame.K_RIGHT] and playerX < screenWidth - 128 - player_vel:
+        playerX += player_vel
+        player_right = True
+        player_left = False
+
+    if keys[pygame.K_UP] and playerY > player_vel:
+        playerY -= player_vel
+        player_up = True
+        player_down = False
+    elif keys[pygame.K_DOWN] and playerY < screenHeight - 128 - player_vel :
+        playerY += player_vel
+        player_up = False
+        player_down = True
+
+    player(playerX, playerY)  # Function for Player_ship will be called here
     pygame.display.update()
+
