173fcda926860dfa71851a0c993b7855b72145d2
juanroder <60340193+juanroder@users.noreply.github.com>
2020-12-10 19:49:26 +0100

    Gegner_Ver_1.0

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 # This ist a Shoot em up game coded by Juan Rodriguez
-import pygame, sys, time
-from pygame.locals import *
+import pygame
+import random  # movement update for enemies.
+
+# from pygame.locals import *
 
 pygame.init()
-FPS = 120
-
-
 # Initialize the Pygame.
 pygame.init()
 
 #  classes
 #  ----------------------------- Objects -------------------------------------
-clock = pygame.time.Clock()
 
+# -------- Background ----------------------
 background = pygame.image.load('BGBig1600.png')
 
 # Create the Screen.
@@ -23,22 +22,36 @@ screen = pygame.display.set_mode((screenWidth, screenHeight))
 # Title and icon on the window bar mas 32px
 pygame.display.set_caption("Razorcop")  # set windows Title.
 icon = pygame.image.load("Ship.png")  # Load tiny icon image
-pygame.display.set_icon(icon)   # set icon.
+pygame.display.set_icon(icon)  # set icon.
 
 # La nave de Razorcop!
 playerImg = pygame.image.load("Player_Ship.png")  # Image Loading
-playerX = 730  # places the x coord. for PlayerImg object in the middle
+playerX = (screenWidth / 2) - (128 / 2)  # places the x coord. for PlayerImg object in the middle
 playerY = 700  # places the y coord. for PlayerImg object in the middle
-playerX_change = 0 # New coordinate point for X
+playerX_change = 0  # New coordinate point for X
 playerY_change = 0  # New coordinate point for y
 player_left = False
 player_right = False
 player_up = False
 player_down = False
-player_vel = 10
+player_vel = 20
+
+# Enemy 1
+enemyImg = pygame.image.load("Gegner.png")  # Image Loading
+enemyX = random.randint(500 - 190, 1100)  # places the x coord. for enemyImg object in the middle
+enemyY = random.randint(50, 150)  # places the y coord. for enemyImg object in the middle
+enemyX_change = 15    # New coordinate point for X
+enemyY_change = 7  # New coordinate point for y
+enemyX_vel = 15
+enemyY_vel = 7
+
 
 def player(x, y):
-    screen.blit(playerImg, (x, y))  # The term used for rendering objects is blitting
+    screen.blit(playerImg, (x, y))  # The term used for rendering objects is Blitting
+
+
+def enemy(x, y):
+    screen.blit(enemyImg, (x, y))  # The term used for rendering objects is Blitting
 
 
 # The window will close in few seconds, we need a while loop (or game loop).
@@ -48,7 +61,7 @@ def player(x, y):
 # Game Loop
 running = True
 while running:  # While running ist True, window stay open.
-    clock.tick(FPS)
+
     screen.fill((0, 0, 0))  # Python needs updates from these changes, so we need a method for it.
     # Background Image
     screen.blit(background, (0, 0))
@@ -72,11 +85,25 @@ while running:  # While running ist True, window stay open.
         playerY -= player_vel
         player_up = True
         player_down = False
-    elif keys[pygame.K_DOWN] and playerY < screenHeight - 128 - player_vel :
+    elif keys[pygame.K_DOWN] and playerY < screenHeight - 128 - player_vel:
         playerY += player_vel
         player_up = False
         player_down = True
 
-    player(playerX, playerY)  # Function for Player_ship will be called here
-    pygame.display.update()
+    enemyX += enemyX_change
+    if enemyX <= 300:
+        enemyX_change = enemyX_vel
+    elif enemyX >= 1200 - 360:
+        enemyX_change = -enemyX_vel
 
+    enemyY += enemyY_change
+    if enemyY <= 10:
+        enemyY_change = enemyY_vel
+    elif enemyY >= 100:
+        enemyY_change = -enemyY_vel
+
+    player(playerX, playerY)  # Function for Player_ship will be called here
+
+    enemy(enemyX, enemyY)  # Function for Enemy  will be called here
+
+    pygame.display.update()
