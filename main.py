47b051023885936298305cc9dfa835687510c767
juanroder <60340193+juanroder@users.noreply.github.com>
2020-12-21 14:14:17 +0100

    first feature player with classes.

--- main.py
+++ main.py
@@ -18,16 +18,22 @@ BLUE = (0, 0, 255)
 # initialize Pygame and create window
 pygame.init()  # start pygame library
 pygame.mixer.init()  # start sounds library
-screen = pygame.display.set_mode((WIDTH, HEIGHT))  # define window's size
-pygame.display.set_caption("Razorcop")  # Name on the windows bar
+screen = pygame.display.set_mode((WIDTH, HEIGHT))  # define windows and window's size
+pygame.display.set_caption("Razorcop")  # name on the windows bar
+icon = pygame.image.load("icon_Ship.png")  # load icon image
+pygame.display.set_icon(icon)
 icon = pygame.image.load("icon_Ship.png")
 clock = pygame.time.Clock()  # create an object to help track time
+
+
+all_sprites = pygame.sprite.Group()  # Object with name Group for all the sprites
+
 # Game Loop
 
 running = True
 while running:
     # Keep loop running at the right speed
-    clock.tick(FPS)
+    clock.tick(FPS)  # tells pygame to figure out how long the loop took.
 
     # Process input (events)
     for event in pygame.event.get():  # track for events inside the loop (keys events per example)
@@ -35,8 +41,11 @@ while running:
             running = False
 
     # Updates
+    all_sprites.update()  # updates sprites
+
     # Draw / Render
     screen.fill(BLACK)
+    all_sprites.draw(screen)  # draws sprites group on the "screen" variable
     pygame.display.flip()  # Double Buffering refresh optimization (after drawing everything flips the display).
 
 pygame.quit()
